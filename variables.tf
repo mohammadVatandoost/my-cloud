89c57216b0b1a45761b20b5b1a7d1d42abe54315
mohammadVatandoost <mohamadvatandoost512@gmail.com>
2023-04-30 00:27:24 +0200

    use k8s provider  2

--- variables.tf
+++ variables.tf
@@ -7,3 +7,11 @@ variable "cluster_name" {
      type= string 
      default = ""
 } 
+
+output printimage_pull_secret {
+        value = "Hello, ${var.image_pull_secret}"
+}
+
+output printcluster_name{
+        value = "Hello, ${var.cluster_name}"
+}
