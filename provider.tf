658bc88f5ff66daa004180999683767e49892210
mohammadVatandoost <mohamadvatandoost512@gmail.com>
2023-05-01 11:06:42 +0200

    add remote backend s3

--- provider.tf
+++ provider.tf
@@ -1,10 +1,23 @@
 terraform {
   required_providers {
     kubernetes = {}
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.18.0"
+    }
+  }
+
+  backend "s3" {
+    bucket         	   = "my-ci-cd-store"
+    key                = "state/terraform.tfstate"
+    region         	   = "eu-north-1"
+    encrypt        	   = true
   }
 
   required_version = ">= 1.2.0"
 }
+
 
 provider "kubernetes" {
   config_path    = "~/.kube/config"
